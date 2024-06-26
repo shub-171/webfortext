0918650cdf2d557e95dea6901ba59e4fdaf7b74c
shub-171 <shubhamagrawal14497@gmail.com>
2024-06-26 18:58:19 +0530

    wip

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer(props) {
     return (
         <div className='d-flex shadow-lg justify-content-center' style={{ backgroundColor: props.mode === 'dark' ? '#2a2a2a' : '#e0e0e0', color: props.mode === 'dark' ? 'white' : '#020202' }}>
-            <p className='my-2'>Made With ❤️ By shubham Agrawal</p>
+            <p className='my-2'>Made With ❤️ By Shubham Agrawal</p>
         </div>
     )
 }
