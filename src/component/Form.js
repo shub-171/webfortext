bdcd2e87c50c24d547d5b2b2c7fa5ff07152c813
shub-171 <shubhamagrawal14497@gmail.com>
2024-07-23 23:48:52 +0530

    Added New Feature

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,51 +1,97 @@
 import React, { useState } from 'react'
+import { LoremIpsum } from 'lorem-ipsum';
 
 function Form(props) {
+
+    const lorem = new LoremIpsum({
+        sentencesPerParagraph: {
+            max: 10,
+            min: 5
+        },
+        wordsPerSentence: {
+            max: 16,
+            min: 8
+        }
+    });
+
     //Upper case
     const upCase = () => {
         let newText = text.toUpperCase();
         setText(newText)
     }
+
     //Lower case
     const loCase = () => {
         let newText = text.toLowerCase();
         setText(newText)
     }
+
     //clear text
     const clear = () => {
         let newText = "";
         setText(newText)
     }
-    //Speak text
-    const speak = () => {
-        let msg = new SpeechSynthesisUtterance();
-        msg.text = text;
-        window.speechSynthesis.speak(msg);
-    }
+
     //Copy text
     const copyText = () => {
         var mytext = document.getElementById("textBox");
         mytext.select();
         navigator.clipboard.writeText(mytext.value);
     }
+
     //Remove extra spaces
     const removeSpace = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
     }
-    //Copy edited text
-    const copyEditedText = () => {
-        const str = document.getElementById('editedText').innerText
-        const el = document.createElement('textarea')
-        el.value = str
-        el.setAttribute('readonly', '')
-        el.style.position = 'absolute'
-        el.style.left = '-9999px'
-        document.body.appendChild(el)
-        el.select()
-        document.execCommand('copy')
-        document.body.removeChild(el)
+
+    //capitalize text
+    const capitalize = () => {
+        const capitalizedText = text.toLowerCase()
+            .split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ');
+        setText(capitalizedText);
+    };
+
+    //String to slug
+    const createSlug = () => {
+        let slug = text
+            .split('\n')
+            .map(line =>
+                line.toLowerCase()
+                    .trim()
+                    .replace(/[^\w\s-]/g, '')
+                    .replace(/\s+/g, '-')
+                    .replace(/-+/g, '-')
+                    .replace(/^-+/, '')
+                    .replace(/-+$/, '')
+            )
+            .join('\n');
+        setText(slug);
+    };
+
+    // remove extra lines
+    const removeEmptyLines = () => {
+        let newText = text.split('\n').filter(line => line.trim() !== '').join('\n');
+        setText(newText)
     }
+
+    // Add line number
+    const addLineNumber = () => {
+        const numbered = text
+            .split('\n')
+            .map((line, index) => `${index + 1}: ${line}`)
+            .join('\n');
+        setText(numbered);
+    };
+
+    // Filler Text
+    const fillerText = () => {
+        const randomText = lorem.generateParagraphs(1); // Generate 5 paragraphs of Lorem Ipsum text
+        setText(randomText);
+    };
+
 
     const handleUp = (event) => {
         setText(event.target.value)
@@ -54,26 +100,39 @@ function Form(props) {
     const [text, setText] = useState("");
 
     return (
-        <div>
-            <div className="my-3 container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+        <>
+            <div className="my-5 mt-3 container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>Type your text here to transform the way you want!!</h1>
-                <textarea className="form-control border-secondary mt-3" value={text} onChange={handleUp} id="textBox" rows="6" style={{ backgroundColor: props.mode === 'dark' ? '#1c1c1c6b' : 'white', color: props.mode === 'dark' ? 'white' : '#020202' }}></textarea>
+                <textarea className="form-control border-secondary mt-3" value={text} onChange={handleUp} id="textBox" rows="12" style={{ backgroundColor: props.mode === 'dark' ? '#1c1c1c6b' : 'white', color: props.mode === 'dark' ? 'white' : '#020202' }}></textarea>
                 <p className='mx-1'>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} characters.</p>
+                <div className='edit-buttons'>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={copyText}>Copy</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={clear}>Clear</button>
 
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={copyText}>Copy Text</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={upCase}>Upper Case</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={loCase}>Lower Case</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={speak}>Speak</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={removeSpace}>Remove extra space</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={clear}>Clear</button>
+                    <div className="dropdown mx-1 mt-2">
+                        <button disabled={text.length === 0} className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Convert Case
+                        </button>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><button disabled={text.length === 0} className="dropdown-item" onClick={upCase}>Upper Case</button></li>
+                            <li><button disabled={text.length === 0} className="dropdown-item" onClick={loCase}>Lower Case</button></li>
+                            <li><button disabled={text.length === 0} className="dropdown-item" onClick={capitalize}>Capitalize</button></li>
+                        </ul>
+                    </div>
 
-                <h3 className="mt-4">Preview Text...</h3>
-                <div className="border border-secondary p-2 rounded" style={{ backgroundColor: props.mode === 'dark' ? '#1c1c1c6b' : 'white', color: props.mode === 'dark' ? 'white' : '#020202' }}>
-                    <p id='editedText'>{text.length > 0 ? text : "Enter text to preview"}</p>
+
+
+
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={removeSpace}>Remove Extra Space</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={removeEmptyLines}>Remove Empty Lines</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={addLineNumber}>Add line number</button>
+                    <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={createSlug}>String to Slug</button>
+                    <button className="btn btn-primary mx-1 mt-2" onClick={fillerText}>Filler Text</button>
                 </div>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 mt-2" onClick={copyEditedText}>Copy Minified Text</button>
             </div>
-        </div>
+
+            <hr className='container' style={{ color: props.mode === 'dark' ? '#777777' : '#1c1c1c' }} />
+        </>
     )
 }
 
