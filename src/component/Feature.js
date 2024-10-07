06a08933170a164b8e278924aa33d415fe237c11
shub-171 <shubhamagrawal14497@gmail.com>
2024-10-07 19:55:24 +0530

    hanges in features

--- src/component/Feature.js
+++ src/component/Feature.js
@@ -10,9 +10,9 @@ function Feature(props) {
                         <div className="col-lg-4 col-sm-6"  key={feature.id} >
                             <div className="card border-0 shadow rounded-lg text-start px-2" style={{minHeight:"300px", backgroundColor: props.mode === 'dark' ? '#2a2a2a' : '#eeeeee80', color: props.mode === 'dark' ? '#f1f1f1' : '#1c1c1c' }}>
                                 <div className="card-body px py-5" >
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="70px" height="70px" fill="currentColor" className={feature.svgClass} viewBox="0 0 16 16" style={{ color: "#0000FF" }}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="70px" height="70px" fill="btn-primary" className={feature.svgClass} viewBox="0 0 16 16" style={{ fill: "#0d6efd" }}>
                                         <path d={feature.svgPath} />
-                                        <path d={feature.svgPath2   } />
+                                        <path d={feature.svgPath2} />
                                     </svg>
                                     <h3 className="h5 my-3">{feature.heading}</h3>
                                     <p className="text-sm mb-0 text-muted">{feature.description}</p>
